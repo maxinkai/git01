de0bddc72960524979bb768f0341844f9ec97898
maxinkai <maxinkai@163.com>
2022-12-01 15:32:01 +0800

    123

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -2,7 +2,7 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("12");
         System.out.println("456");
-        System.out.println("7891111");
-        System.out.println("789222");
+        System.out.println("789");
+        System.out.println("789");
     }
 }
